aa46157e9655b51ba21ff33851c901e41c7b2774
waferhsiao <1586673525@qq.com>
2024-04-30 17:56:06 +0800

    gateway配置 -6

---
+++ wafer_gateway/src/main/java/com/wafer/business/Main8082.java
@@ -0,0 +1,12 @@
+package com.wafer.business;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main8082 {
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Main8082.class,args);
+    }
+}
