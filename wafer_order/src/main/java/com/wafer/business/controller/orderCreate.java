b9ced048c0fa6b7fff0dc25434015f0cfa6abe7d
waferhsiao <1586673525@qq.com>
2024-05-06 11:10:56 +0800

    gateway配置 -7

--- wafer_order/src/main/java/com/wafer/business/controller/orderCreate.java
+++ wafer_order/src/main/java/com/wafer/business/controller/orderCreate.java
@@ -1,10 +1,24 @@
 package com.wafer.business.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.Arrays;
 
+@RestController
+@RequestMapping("/order")
 public class orderCreate {
+
+    @Value("${server.port}")
+    String portValue;
 
     public void orderAdd(){
 
     }
+
+    @GetMapping("/detail")
+    public String getOrderDetail(@RequestParam String orderId){
+        return "orderId:"+orderId  + ";port："+portValue;
+    }
+
 }
